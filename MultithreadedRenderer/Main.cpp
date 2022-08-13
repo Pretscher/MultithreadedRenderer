98c9bd3bf95f98ac6ab7586efedf25cb39ee763b
Julian Kollenbroich <julian.kollenbroich@gmail.com>
2022-08-13 16:06:26 +0200

    some documentation and additional testing for the main program

--- MultithreadedRenderer/Main.cpp
+++ MultithreadedRenderer/Main.cpp
@@ -12,14 +12,14 @@ ts::Line *line;
 ts::Text *text;
 
 void init() {
-  
-    rect2 = (new ts::Rect(0, 210, 200, 200))->addOutline(sf::Color::Red, 10)->setColor(sf::Color::Blue);
+    rect = (new ts::Rect(0, 0, 200, 200))->addTexture("myRecources/Textures/container.jpg", true)->setPriority(2);//should not be overdrawn by second rect
+    rect2 = (new ts::Rect(0, 100, 200, 200))->addOutline(sf::Color::Red, 10)->setColor(sf::Color::Blue);
     circle = (new ts::Circle(1000, 200, 50))->setColor(sf::Color::Green)->addOutline(sf::Color::Yellow, 5);
     circle2 = (new ts::Circle(500, 200, 50))->addTexture("myRecources/Textures/awesomeface.png", true);
     line = (new ts::Line(1600, 0, 1600, 1080))->setColor(sf::Color::Red);
     text = (new ts::Text(1600, 0, "A glorious Text!"))->setColor(sf::Color::Green);
-    rect = (new ts::Rect(0, 0, 1920, 1080))->addTexture("myRecources/Textures/container.jpg", true)->setPriority(0);
-    //Renderer::addBackground("myRecources/Textures/game-background-hills.jpg", true);
+
+    Renderer::addBackground("myRecources/Textures/game-background-hills.jpg", true);//although called last, has priority 0 => is background
 }
 
 float x;
@@ -32,15 +32,19 @@ void eventloop() {
     }
     if(x >= 300) {
         goRight = false;
-
+		//check if deleting a shape removes it from the renderer properly
+        if (circle != nullptr) {
+            delete circle;
+            circle = nullptr;
+        }
 
     } else if(x <= 10) {
         goRight = true;
     }
-    circle->setRadius(x);
+  //  circle->setRadius(x);
     circle2->setRadius(300 - x);
-   // rect->transform(x, 0);
-    rect2->transform(x, 210);
+    rect->transform(x, 0);
+    rect2->transform(x, 100);
 }
 
 int main() {
@@ -50,7 +54,7 @@ int main() {
     Renderer::initWindow();
     sf::Event events;
     while(Renderer::window->isOpen()) {
-        this_thread::sleep_for(1ms);
+      //  this_thread::sleep_for(1ms);
         while (Renderer::window->pollEvent(events)) {
             if (events.type == sf::Event::Closed) {
                 Renderer::window->close();
